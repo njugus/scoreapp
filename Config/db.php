1cac2373f9308738480dae5703fe70dfae4a6ac2
njugus <kayvohnjuguna@gmail.com>
2025-05-26 18:42:20 -0700

    adds database credebtials for connection purposes

---
+++ Config/db.php
@@ -0,0 +1,6 @@
+<?php
+define('DB_HOST', 'sqlXXX.epizy.com'); // From InfinityFree
+define('DB_USER', 'epiz_123456');      // Your MySQL username
+define('DB_PASS', 'yourpassword');     // Your MySQL password
+define('DB_NAME', 'epiz_123456_judging_system');
+?>
